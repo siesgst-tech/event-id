bcf18defb755a24017a07bdfdb5694919838cfa6
vinaya8 <darkmatter865@gmail.com>
2017-10-16 00:08:54 +0530

    download btn removed

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -18,9 +18,7 @@
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#"><button class="btn btn-sm btn-primary"><i class="fa fa-android" style="font-size: 1.2em;"></i> Download Event ID</button></a>
-                    </li>
+                    
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
                     @if (Request::is('auth/*'))
